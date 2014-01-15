124df490885bf33a09d127fcf58b1593062c8c1e
sosloow <sosloow@gmail.com>
2014-01-15 17:16:35 +0600

    such commit wow

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+China::Application.config.secret_key_base = 'e7cfe72fc6bcf3a964e3579d9c642a37022276318182c03c7a49d78f171c52b25dc327bdf8c1200b707ff5f645f919e2749cd71e284fadf3c32c2a84f0eefddf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+China::Application.config.session_store :cookie_store, key: '_china_session'
