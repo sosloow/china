8007e48fa8c57c2cd70f9e74efd38b8ba0f8b22d
sosloow <sosloow@gmail.com>
2014-01-15 21:13:28 +0600

    localization

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 China::Application.routes.draw do
+  
+  
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+  devise_for :users
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
