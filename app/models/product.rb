81cbba0afb5ee3fffa284dc34ee9dd576f5c90bd
Sa7cez <sa7cez@gmail.com>
2014-02-03 19:27:42 +0600

    image on products fixed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,14 @@
 class Product < ActiveRecord::Base
-  has_attached_file :image
+
+  has_attached_file :image, styles: {medium: '115x115>'}
 
   attr_accessor :delete_image
   before_validation { self.image.clear if self.delete_image == '1' }
 
   validates :title, presence: true
   validates :description, presence: true
-  validates_attachment :image, content_type: {content_type:
-    ["image/jpg", "image/gif", "image/png"]}
+  #validates_attachment :image, content_type: {content_type:
+    #["image/jpg", "image/gif", "image/png"]}
 
   belongs_to :sub_category
   belongs_to :brand
