8b7872707a479bbd017854d9707436be6a4b3609
Sosloow <sosloow@gmail.com>
2014-09-03 03:03:51 +0600

    nvm

--- config/deploy.rb
+++ config/deploy.rb
@@ -120,4 +120,12 @@ namespace :deploy do
   end
 end
 
+namespace :rake do  
+  desc "Run a task on a remote server."  
+  # run like: cap staging rake:invoke task=a_certain_task  
+  task :invoke do  
+    run("cd #{deploy_to}/current; /usr/bin/env rake #{ENV['task']} RAILS_ENV=#{rails_env}")  
+  end  
+end
+
 after 'deploy', 'deploy:migrate' #, 'deploy:seed'
